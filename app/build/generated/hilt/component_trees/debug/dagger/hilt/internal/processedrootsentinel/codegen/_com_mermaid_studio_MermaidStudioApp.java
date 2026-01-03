3b8f5f365913a9b3e078fe304f42abe21cd365c5
siyongshuai <siyongshuai@gmail.com>
2026-01-04 02:39:25 +0800

    Add .gitignore and remove build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_mermaid_studio_MermaidStudioApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.mermaid.studio.MermaidStudioApp"
-)
-public final class _com_mermaid_studio_MermaidStudioApp {
-}
